9ee704c25f21e73561e3c48295753eef09a40fb0
AmrutaFT <amrutam@futurismtechnologies.com>
2024-12-18 06:48:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =2215
+let accounEmail = "hfd@gmail.com"
+var accountPassword ="12345"
+accountCity = "pune"
+let accountStates 
+// accountId = 2
+
+accounEmail ="behfbh@yahoo.com"
+accountPassword="65456"
+accountCity="mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accounEmail, accountPassword, accountCity, accountStates]);
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  //traet all js code as newer version
+
+// alert(3+3)  //we are using node js, not browser
+console.log(3+3);
+console.log("hitesh");
+
+
+let name="hitesh"
+let age = 18
+let  isLoggedIn = false
+
+//primitive
+//number
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
