128be70d70bb09bec7e73a8b2aff92e475905fa8
AmrutaFT <amrutam@futurismtechnologies.com>
2025-01-29 19:20:12 +0530

    Add files via upload

---
+++ 02_arrays/02_arrays.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["spiderman", "flash", "batman"]
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
